e46e2352b4e6a69f4351c17ec5e29f08873dbdd7
mac <237774366+shumsterm406-oss@users.noreply.github.com>
2026-01-16 20:35:11 +0600

    add Product model with line serialization

--- product.py
+++ product.py
@@ -1,13 +1,16 @@
 # Модель для товаров (ООП)
 class Product:
-    def __init__(self, name: str, price: int):
+    def __init__(self, name: str, price: float):
         self.name = name
         self.price = price
 
     @staticmethod
     def from_line(line: str):
-        name, price = line.strip('—')
-        return Product(name.strip(), int(price.strip()))
+        line = line.replace("—", "-")
+        parts = line.split("-")
+        if len(parts) != 2:
+            raise ValueError(f"Неправильная строка: {line}")
+        return Product(parts[0].strip(), float(parts[1].strip()))
 
     def to_line(self):
         return f"{self.name} — {self.price}"
